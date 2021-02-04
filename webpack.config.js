7ee263e46081d87713b8bdafa9f79b99a27d1052
Derrick Plotsky <derrickplotsky@gmail.com>
2021-02-03 18:30:09 -0800

    Add our webpack config
    It's a config. For webpack.

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+  entry: './src/index.ts',
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Output Management',
+    }),
+    new CopyPlugin({
+      patterns: [
+        { from: "src/glsl", to: "glsl" },
+        { from: "src/assets", to: "assets" }
+      ],
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      }
+    ],
+  },
+  resolve: {
+    extensions: [ '.tsx', '.ts', '.js' ],
+  },
+  devServer: {
+    contentBase: './dist',
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+};
