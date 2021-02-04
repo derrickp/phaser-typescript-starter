b786d8e0a8d06cf46bed29d87814e10e67f0b935
Derrick Plotsky <derrickplotsky@gmail.com>
2021-02-03 18:30:28 -0800

    Mostly copy the rollup starter
    This is a _super_ basic "game" that just bounces some text on the
    screen.
    
    But it does show how you can setup assets and scripts.

---
+++ src/index.ts
@@ -0,0 +1,46 @@
+import "phaser";
+
+export default class Demo extends Phaser.Scene
+{
+    constructor ()
+    {
+        super('demo');
+    }
+
+    preload ()
+    {
+        this.load.image('logo', 'assets/phaser3-logo.png');
+        this.load.glsl('bundle', 'glsl/plasma-bundle.glsl.js');
+        this.load.glsl('stars', 'glsl/starfields.glsl.js');
+    }
+
+    create ()
+    {
+        this.add.shader('RGB Shift Field', 0, 0, 800, 600).setOrigin(0);
+
+        this.add.shader('Plasma', 0, 412, 800, 172).setOrigin(0);
+
+        const logo = this.add.image(400, 70, 'logo');
+
+        this.tweens.add({
+            targets: logo,
+            y: 350,
+            duration: 1500,
+            ease: 'Sine.inOut',
+            yoyo: true,
+            repeat: -1
+        })
+    }
+}
+
+const config = {
+    type: Phaser.AUTO,
+    backgroundColor: '#125555',
+    width: 800,
+    height: 600,
+    scene: Demo
+};
+
+(() => {
+  new Phaser.Game(config);
+})();
